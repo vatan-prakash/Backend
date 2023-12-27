7841efb14b72b4c49d89567d847d0c61f27b8002
Vatan Prakash <vatanprakash867@gmail.com>
2023-12-27 17:49:08 +0530

    HELLO

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-app.listen(port, () => {
+app.get('/twi', (req, res) => {
+    res.send('Vatan')
+  })
+app.get('/',(req, res)=>{
+    res.send('')
+})
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
+ 
